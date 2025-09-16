f86a0d5257c57660a039d14d2bbbc3e4f75895e6
Velid Heldic <velid2004@gmail.com>
2025-09-16 23:35:47 +0200

    Project setup

---
+++ Model/Document.cs
@@ -0,0 +1,12 @@
+﻿namespace Model
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Filetype { get; set; }
+        public int ByteSize { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ PaperlessREST/Services/DocumentManagerService.cs
@@ -0,0 +1,28 @@
+﻿using PaperlessREST.Services.Interface;
+using Model;
+
+namespace PaperlessREST.Services
+{
+    public class DocumentManagerService : IDocumentManagerService
+    {
+        public Document GetDocument(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDocument(Document document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDocument(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDocument(int id, Document document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
