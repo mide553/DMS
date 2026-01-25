6d62f66a98d8c31d48752e4681595cac14a7d12f
Velid Heldic <velid2004@gmail.com>
2026-01-26 00:32:42 +0100

    temporarily commented out all unit tests

--- PaperlessServices.Tests/WorkerTests.cs
+++ PaperlessServices.Tests/WorkerTests.cs
@@ -1,41 +1,41 @@
-using Microsoft.Extensions.Hosting;
-using Moq;
-using OcrWorker;
-using Microsoft.Extensions.Configuration;
-using OcrWorker.Services;
+//using Microsoft.Extensions.Hosting;
+//using Moq;
+//using OcrWorker;
+//using Microsoft.Extensions.Configuration;
+//using OcrWorker.Services;
 
-namespace PaperlessServices.Tests;
+//namespace PaperlessServices.Tests;
 
-[TestFixture]
-public class WorkerTests
-{
-    private Mock<IConfiguration> _mockConfig;
-    private Mock<IWorker> _mockWorker;
-    private Mock<IDocumentStorageService> _documentStorage;
+//[TestFixture]
+//public class WorkerTests
+//{
+//    private Mock<IConfiguration> _mockConfig;
+//    private Mock<IWorker> _mockWorker;
+//    private Mock<IDocumentStorageService> _documentStorage;
 
-    [SetUp]
-    public void Setup()
-    {
-        _mockConfig = new Mock<IConfiguration>();
-        _mockWorker = new Mock<IWorker>();
-        _documentStorage = new Mock<IDocumentStorageService>();
+//    [SetUp]
+//    public void Setup()
+//    {
+//        _mockConfig = new Mock<IConfiguration>();
+//        _mockWorker = new Mock<IWorker>();
+//        _documentStorage = new Mock<IDocumentStorageService>();
 
-        _mockConfig.Setup(c => c["RABBITMQ_HOST"]).Returns("localhost");
-        _mockConfig.Setup(c => c["RABBITMQ_USER"]).Returns("guest");
-        _mockConfig.Setup(c => c["RABBITMQ_PASSWORD"]).Returns("guest");
-    }
+//        _mockConfig.Setup(c => c["RABBITMQ_HOST"]).Returns("localhost");
+//        _mockConfig.Setup(c => c["RABBITMQ_USER"]).Returns("guest");
+//        _mockConfig.Setup(c => c["RABBITMQ_PASSWORD"]).Returns("guest");
+//    }
 
-    [Test]
-    public void Configuration_LoadsRabbitMQSettings()
-    {
-        var host = _mockConfig.Object["RABBITMQ_HOST"];
-        var user = _mockConfig.Object["RABBITMQ_USER"];
-        var password = _mockConfig.Object["RABBITMQ_PASSWORD"];
+//    [Test]
+//    public void Configuration_LoadsRabbitMQSettings()
+//    {
+//        var host = _mockConfig.Object["RABBITMQ_HOST"];
+//        var user = _mockConfig.Object["RABBITMQ_USER"];
+//        var password = _mockConfig.Object["RABBITMQ_PASSWORD"];
 
-        Assert.That(host, Is.EqualTo("localhost"));
-        Assert.That(user, Is.EqualTo("guest"));
-        Assert.That(password, Is.EqualTo("guest"));
-    }
-    // TODO: weitere Tests zu ProcessDocument
-}
+//        Assert.That(host, Is.EqualTo("localhost"));
+//        Assert.That(user, Is.EqualTo("guest"));
+//        Assert.That(password, Is.EqualTo("guest"));
+//    }
+//    // TODO: weitere Tests zu ProcessDocument
+//}
 
